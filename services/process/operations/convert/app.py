e01069d4fadbf04ebab89f3c2a9c01ebf68a758b
Gunnar Busch <gunnar.busch@eodc.eu>
2018-03-05 10:12:17 +0100

    Another copy fix

--- services/process/operations/convert/app.py
+++ services/process/operations/convert/app.py
@@ -14,7 +14,7 @@ def copy_data():
     out_dir = "{0}/{1}".format(OUT_VOLUME, PARAMS["output"]["folder"])
 
     for mount in INPUT_MOUNTS:
-        copytree(mount, out_dir)
+        copytree("/" + mount, out_dir)
 
 if __name__ == "__main__":
     print("Start result data copy process...")
