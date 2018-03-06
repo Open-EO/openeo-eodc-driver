44328593f282e554e60c7d72aee718f632a50bbe
sherrmann <sophie.herrmann@eodc.eu>
2018-03-06 16:57:53 +0100

    Min-time process first version

--- services/process/operations/min-time/app.py
+++ services/process/operations/min-time/app.py
@@ -2,44 +2,88 @@
 
 '''
 
-from os import makedirs, listdir
-from zipfile import ZipFile
-from utils import read_parameters, read_input_mounts
-from time import sleep
-from shutil import copyfile, rmtree
-from json import load, dump
+from osgeo import gdal
+import numpy
 
-OUT_VOLUME = "/job_out"
+from utils import read_parameters, read_input_mounts, create_folder
+
+OUT_VOLUME = "/home/sophie/git_repos/data/s2_process/min_time/job_out"
+OUT_FINAL = create_folder(OUT_VOLUME, "out_min-time")
 PARAMS = read_parameters()
 INPUT_MOUNTS = read_input_mounts()
 
 def perform_min_time():
     ''' Performs min time '''
 
-    print("Doing something...")
+    print("-> Start calculating minimal NDVI...")
 
-    processed = []
-    for mount in INPUT_MOUNTS:
-        with open("/{0}/files.json".format(mount), 'r') as json_file:
-            files = load(json_file)
+    # Create dict to store data with date in
+    data = None
+    count = 0
 
-        for file_data in files:
-            copyfile(
-                "{0}/{1}".format(mount, file_data["file_path"]), 
-                "{0}/{1}".format(OUT_VOLUME, file_data["file_name"]))
+    # Define output
+    out_dataset = None
+    out_path = "{0}/min-time_epsg-{1}.tif".format(OUT_FINAL, PARAMS["data_srs"].split(":")[-1])  # TODO Add time range?
 
-            print("Copyed file: " + file_data["file_name"])
+    # Iterate over each file and save the data
+    for file_path in PARAMS["file_paths"]:
 
-            file_data["file_path"] = file_data["file_name"]
-            file_data["operations"].append("min_time")
-            processed.append(file_data)
+        # Get data
+        dataset = gdal.Open(file_path)
+        band = dataset.GetRasterBand(1)
+        file_data = band.ReadAsArray()
 
-    with open("/{0}/files.json".format(OUT_VOLUME), 'w') as outfile:
-        dump(processed, outfile)
-    
-    print("Finished doing something.")
+        if data is None:
+            data = numpy.zeros((len(PARAMS["file_paths"]), dataset.RasterYSize, dataset.RasterXSize))
+
+            # Save dummy out dataset to be filled with data
+            out_dataset = create_out_dataset(dataset, out_path)
+
+        # Save data to dict
+        data[count] = file_data
+
+        count += 1
+
+    min_time_data = numpy.minimum.reduce(data)
+
+    # Write data to dataset
+    fill_dataset(out_dataset, min_time_data)
+
+    print("-> Finished calculating minimal NDVI")
+
+
+def create_out_dataset(in_dataset, out_path):
+    '''Create output dataset'''
+
+    driver = gdal.GetDriverByName("GTiff")
+
+    # Create output file from input (only one band)
+    out_dataset = driver.Create(out_path, in_dataset.RasterXSize, in_dataset.RasterYSize, 1, gdal.GDT_Float32)
+
+    # Add spatial information from input dataset
+    out_dataset.SetGeoTransform(in_dataset.GetGeoTransform())
+    out_dataset.SetProjection(in_dataset.GetProjection())
+
+    # Write to disk
+    out_dataset.FlushCache()
+
+    return out_dataset
+
+
+def fill_dataset(dataset, data):
+    '''Fill empty dataset with data'''
+
+    # Add data
+    band = dataset.GetRasterBand(1)
+    band.WriteArray(data)
+    band.SetNoDataValue(2)
+
+    # Write to disk
+    dataset.FlushCache()
+
 
 if __name__ == "__main__":
     print("Start processing 'min_time' ...")
     perform_min_time()
     print("Finished 'min_time' processing.")
+
--- services/process/operations/min-time/utils.py
+++ services/process/operations/min-time/utils.py
@@ -1,9 +1,11 @@
 ''' Utilities for Sentinel-2 data extraction '''
 
 import json
+from os import path, makedirs
 
 CONFIG_FILE = "/job_config/config.json"
 IN_MOUNTS_FILE = "/job_config/input_mounts.json"
+
 
 def read_parameters():
     ''' Return parameters from config file '''
@@ -13,6 +15,7 @@ def read_parameters():
 
     return parameters
 
+
 def read_input_mounts():
     ''' Return parameters from config file '''
 
@@ -20,3 +23,12 @@ def read_input_mounts():
         input_mounts = json.load(json_file)
 
     return input_mounts
+
+
+def create_folder(base_folder, new_folder):
+    '''Creates new_folder inside base_folder if it does not exist'''
+
+    folder_path = "{0}/{1}".format(base_folder, new_folder)
+    if not path.exists(folder_path):
+        makedirs(folder_path)
+    return folder_path
