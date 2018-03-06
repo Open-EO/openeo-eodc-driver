d75c1bd102e56923e8cdb59f3ef8395546d89fbb
sherrmann <sophie.herrmann@eodc.eu>
2018-03-06 15:18:03 +0100

    NDVI process working

--- services/process/operations/ndvi/utils.py
+++ services/process/operations/ndvi/utils.py
@@ -1,9 +1,11 @@
 ''' Utilities for Sentinel-2 data extraction '''
 
 import json
+from os import path, makedirs, listdir
 
 CONFIG_FILE = "/job_config/config.json"
 IN_MOUNTS_FILE = "/job_config/input_mounts.json"
+
 
 def read_parameters():
     ''' Return parameters from config file '''
@@ -13,6 +15,7 @@ def read_parameters():
 
     return parameters
 
+
 def read_input_mounts():
     ''' Return parameters from config file '''
 
@@ -20,3 +23,27 @@ def read_input_mounts():
         input_mounts = json.load(json_file)
 
     return input_mounts
+
+
+def create_folder(base_folder, new_folder):
+    '''Creates new_folder inside base_folder if it does not exist'''
+
+    folder_path = "{0}/{1}".format(base_folder, new_folder)
+    if not path.exists(folder_path):
+        makedirs(folder_path)
+    return folder_path
+
+
+def write_output_to_json(data, operation_name, folder):
+    '''Creates folder out_config in folder and writes data to json inside this new folder'''
+    folder_out_config = create_folder(folder, "out_config")
+
+    with open("{0}/out_{1}_config.json".format(folder_out_config, operation_name), "w") as outfile:
+        json.dump(data, outfile)
+
+
+def get_paths_for_files_in_folder(folder_path):
+    '''Returns a list of all file paths inside the given folder'''
+
+    file_list = listdir(folder_path)
+    return [folder_path + "/" + file for file in file_list]
