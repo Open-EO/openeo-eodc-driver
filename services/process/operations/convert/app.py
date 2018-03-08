e93c949c6222c874f1546eebb6274a3a030d05c4
Gunnar Busch <gunnar.busch@eodc.eu>
2018-03-08 19:20:21 +0100

    Result copy fix

--- services/process/operations/convert/app.py
+++ services/process/operations/convert/app.py
@@ -12,13 +12,17 @@ def copy_data():
     ''' Copies the data into the job result folder '''
 
     out_dir = "{0}/{1}".format(OUT_VOLUME, PARAMS["output"]["folder"])
+
+    if not path.exists(out_dir):
+        makedirs(out_dir)
+
     for mount in INPUT_MOUNTS:
         with open("{0}/files.json".format(mount), 'r') as json_file:
             file_paths = load(json_file)["file_paths"]
 
         for file_path in file_paths:
             file_path = "{0}/{1}".format(mount, file_path)
-            file_name = file_path.split("/")[1]
+            file_name = file_path.split("/")[-1]
             copyfile(file_path, "{0}/{1}".format(out_dir, file_name))
             print(" -> Copied file: " + file_path)
 
