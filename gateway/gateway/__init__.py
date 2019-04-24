1ae199efcb1f196a4765f63976e2e90ae45a5b6e
Luca Foresta <luca.foresta@gmail.com>
2019-04-24 14:39:37 +0200

    added comment

--- gateway/gateway/__init__.py
+++ gateway/gateway/__init__.py
@@ -52,7 +52,7 @@ with ctx:
     gateway.add_endpoint("/process_graphs/<process_graph_id>", func=rpc.process_graphs.delete, auth=True, validate=True, methods=["DELETE"])
 
     # Job Management
-    # /output_formats
+    # /output_formats -> implemented under 'Capabilities'
     # /preview
     gateway.add_endpoint("/jobs", func=rpc.jobs.get_all, auth=True, validate=True)
     gateway.add_endpoint("/jobs", func=rpc.jobs.create, auth=True, validate=True, methods=["POST"])
