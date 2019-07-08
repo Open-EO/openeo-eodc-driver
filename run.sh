ccdf496d99738d642903103b7f1c60aef95c25eb
Selwyn Rogers <selwyn.rogers@eodc.eu>
2019-07-08 10:59:11 +0200

    add info output in run script

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ echo " "
 echo " "
 
 check_container () {
-  echo "Checking if container ${1} exists."
+  echo "#################### Checking if container ${1} exists. ####################"
   if [ ! "$(docker ps -q -f name=${1})" ]; then
     echo "Container ${1} exists."
     echo "Checking container ${1} status"
@@ -28,22 +28,24 @@ check_container () {
 } 
 
 check_image () {
-  echo "Checking if image ${1} exits."
+  echo "#################### Checking if image ${1} exits. ####################"
   if [[ "$(docker images -q ${1} 2> /dev/null)" == "" ]]; then
-    echo "Building ${1} image."
+    echo "#################### Building ${1} image. ####################"
     docker build ${2}
   else
-    echo "Image ${2} exists."
+    echo "#################### Image ${2} exists. ####################"
   fi
 }
 
 if [ ! -f ./.init ]; then
+  echo "#################### Starting initial setup. ####################"
   docker-compose -f docker-compose.nginx.yml up -d --build \
   && check_container nginx \
   && check_image openeo-base ./base \
   && docker-compose -f docker-compose.yml up -d --build \
   && touch .init
 else
+  echo "#################### Found .init file. Starting existing containers. ####################"
   docker-compose -f docker-compose.nginx.yml up -d \
   && check_container nginx \
   && docker-compose -f docker-compose.yml up -d
