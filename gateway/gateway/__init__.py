851ed5f666e505f61bb602c4110f0bf8c16453f1
sherrmann <sophie.herrmann@eodc.eu>
2019-07-09 17:53:00 +0200

    Make /collections and /processes endpoint publicly available
    No authentication needed
    
    Signed-off-by: sherrmann <sophie.herrmann@eodc.eu>

--- gateway/gateway/__init__.py
+++ gateway/gateway/__init__.py
@@ -20,14 +20,14 @@ with ctx:
     # /service_types
 
     # EO Data Discovery
-    gateway.add_endpoint("/collections", func=rpc.data.get_all_products, auth=True, validate=True)
-    gateway.add_endpoint("/collections/<name>", func=rpc.data.get_product_detail, auth=True, validate=True)
+    gateway.add_endpoint("/collections", func=rpc.data.get_all_products, auth=False, validate=True)
+    gateway.add_endpoint("/collections/<name>", func=rpc.data.get_product_detail, auth=False, validate=True)
     # NB following route should become a process, and not be available as a route
-    gateway.add_endpoint("/collections/<name>/records", func=rpc.data.get_records, auth=True, validate=True) # NB extension of openEO API
+    gateway.add_endpoint("/collections/<name>/records", func=rpc.data.get_records, auth=False, validate=True) # NB extension of openEO API
     # /subscription
 
     # Process Discovery
-    gateway.add_endpoint("/processes", func=rpc.processes.get_all, auth=True, validate=True)
+    gateway.add_endpoint("/processes", func=rpc.processes.get_all, auth=False, validate=True)
     gateway.add_endpoint("/processes", func=rpc.processes.create, auth=True, validate=True, methods=["POST"], role="admin") # NB extension of openEO API
 
     # Account Management
