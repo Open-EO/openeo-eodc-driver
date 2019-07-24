e6d1fb3b453ca0229bbb05d437f4977fa87597e7
lforesta <luca.foresta@eodc.eu>
2019-07-24 08:47:18 +0200

    Jobs airflow (#29)
    * function to convert openeo syntax
    
    - map_processes maps openeo syntayt to eodatareaders
    - writeairflowdag embeds calls to eodatareaders into an apache airflow
    dag
    
    * create() generates job as airflow dag
    
    * refresh dags every 30sec (not 300)
    
    * added env vars and volumes
    
    * updated
    
    * excludes dags from git
    
    * airflow in same docker network as openeo API
    
    - only needed for local development. If not in the same network, it is not possible to connect to the airflow webserver from the openeo services
    
    * refresh dags folder every 1 sec
    
    * added project_name to env vars
    
    - must be the same as in openeo api docker-compose
    
    * instructions to set AIRFLOW_HOST var for local dev
    
    * added variable (to be set manually for local dev)
    
    * dags folder specified with env var in container
    
    * updated paths were to store dag and processed data
    
    * calss to use airflow api
    
    * jobs service uses airflow api
    
    * openshift related, deprecated
    
    * clean up
    
    * added method
    
    * adds / after env var AIRFLOW_DAGS

---
+++ services/jobs/jobs/dependencies/airflow_conn.py
@@ -0,0 +1,58 @@
+from os import environ
+import requests
+
+class Airflow():
+    """
+
+    """
+    
+    def __init__(self):
+        """
+        
+        """
+
+        self.api_url = environ.get('AIRFLOW_HOST') + "/api/experimental"
+        self.dags_url = environ.get('AIRFLOW_HOST') + "/api/experimental/dags"
+        self.header = {'Cache-Control': 'no-cache ', 'content-type': 'application/json'}
+        self.data = '{}'
+        
+    def check_api(self):
+        """
+        
+        """
+        
+        response = requests.get(self.api_url + "/test")
+        
+        return response
+
+    def unpause_dag(self, job_id, unpause=True):
+        """
+        Pause/unpause DAG
+        """
+
+        request_url = self.dags_url + "/" + job_id + "/paused/" + str(unpause)
+        response = requests.get(request_url, headers=self.header, data=self.data)
+
+        return response
+          
+          
+    def trigger_dag(self, job_id):
+        """
+        Trigger airflow DAG (only works if it is unpaused already)
+        """
+
+        job_url = self.dags_url + "/" + job_id + "/dag_runs"
+        response = requests.post(job_url, headers=self.header, data=self.data)
+
+        return response
+      
+
+    def check_dag_status(self, job_id):
+        """
+        Check status of airflow DAG
+        """
+
+        job_url = self.dags_url + "/" + job_id + "/dag_runs"
+        response = requests.get(job_url, headers=self.header, data=self.data)
+
+        return response
