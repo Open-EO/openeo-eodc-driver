4691f8fa571d0cb6e2b5d0ad1a28c9d14623508d
Luca Foresta <luca.foresta@gmail.com>
2019-09-17 11:11:16 +0200

    cleanup

--- airflow/plugins/eodatareaders_operators.py
+++ airflow/plugins/eodatareaders_operators.py
@@ -13,17 +13,12 @@ class eoDataReadersOp(BaseOperator):
 
     @apply_defaults
     def __init__(self, input_filepaths, input_params, *args, **kwargs):
-        #self.operator_param = my_operator_param
         self.input_filepaths = input_filepaths
         self.input_params = input_params
         super(eoDataReadersOp, self).__init__(*args, **kwargs)
 
-    #def execute(self, context, input_filepaths, params=None):
     def execute(self, context):
         # If folder is given, list files in folder
-        # print('####')
-        # print(self.input_filepaths, type(self.input_filepaths))
-        # print('####')
         if isinstance(self.input_filepaths, str):
             if os.path.isdir(self.input_filepaths):
                 # input string is a directory, list all its files
@@ -41,12 +36,7 @@ class eoDataReadersOp(BaseOperator):
             else:
                 # input list is list of strings (filepaths)
                 filepaths = self.input_filepaths
-        # print('+++++++++++')
-        # print(filepaths)
-        # print('+++++++++++')
         _ = eoDataReader(filepaths, self.input_params)
-        #log.info("Hello World!")
-        #log.info('operator_param: %s', self.operator_param)
 
 class eoDataReadersPlugin(AirflowPlugin):
     name = "eo_data_readers_plugin"
