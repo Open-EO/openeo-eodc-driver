5848dce24d77006342f07bbff4681f1b30d4cd90
Luca Foresta <luca.foresta@gmail.com>
2019-09-23 18:06:46 +0200

    only id_tokens allowed
    - id_tokens are verified against relative oidc provider
    - added possibility to use oidc providers other than EODC ones
    - external users must be manually added to a list of allowed users

--- gateway/gateway/dependencies/auth.py
+++ gateway/gateway/dependencies/auth.py
@@ -1,16 +1,14 @@
 """ AuthenticationHandler """
 
-from os import environ
-from flask import request, g
-from flask.wrappers import Request, Response
-#from flask_oidc import OpenIDConnect
-from flask_nameko import FlaskPooledClusterRpcProxy
-from requests import post, exceptions
-from jwt import decode
-from typing import Callable, Union
-from keycloak import KeycloakOpenID
-from keycloak.exceptions import KeycloakAuthenticationError
 
+import os
+from flask import request
+from flask.wrappers import Request, Response
+import jwt
+import json
+import requests
+from typing import Union
+from .jwksutils import rsa_pem_from_jwk
 from .response import ResponseParser, APIException
 
 
@@ -21,67 +19,27 @@ class AuthenticationHandler:
 
     def __init__(self, response_handler: ResponseParser):
         self._res = response_handler
-
-        # Instantiate keycloak client
-        self.keycloak_openid = start_keycloak()
-
+        
+        
     def oidc_token(self, func):
         def decorator(*args, **kwargs):
             try:
                 token = self._parse_auth_header(request)
-                userinfo = self.keycloak_openid.userinfo(token)
-                if userinfo['email_verified']:
-                    user_id = userinfo['sub']
+                verified = verify_token(token)
+                user_id = verified['sub']
+                if verified['email_verified']:
                     decorated_function = func(user_id=user_id)
                     return decorated_function
                 else:
                     raise APIException(
-                        msg="The email address of user {0} is not verified."\
+                        msg="The email address of user {0} is not verified by the identity provider."\
                             .format(user_id),
                         code=401,
                         service="gateway",
                         internal=False)
-            except KeycloakAuthenticationError as exc:
-                from ast import literal_eval
-                msg = literal_eval(exc.error_message.decode('utf-8'))['error_description']
-                return self._res.error(
-                    APIException(
-                        msg=msg,
-                        code=exc.response_code,
-                        service="gateway",
-                        internal=False
-                    )
-                )
             except Exception as exc:
-                return self._res.error(exc)
+                return self._res.error(exc)        
         return decorator
-
-    # def oidc(self, f):
-    #     def decorator(*args, **kwargs):
-    #         try:
-    #             if g.oidc_id_token is None:
-    #                 return self._oidc.redirect_to_auth_server(request.url)
-    #
-    #             user_id = self._oidc.user_getfield("sub")
-    #
-    #             if not self._oidc.user_getfield("email_verified"):
-    #                 raise APIException(
-    #                     msg="The email address of user {0} is not verified."\
-    #                         .format(user_id),
-    #                     code=401,
-    #                     service="gateway",
-    #                     internal=False)
-    #
-    #             return f(user_id=user_id)
-    #         except exceptions.HTTPError as exc:
-    #             raise APIException(
-    #                 msg=str(exc),
-    #                 code=401,
-    #                 service="gateway",
-    #                 internal=False)
-    #         except Exception as exc:
-    #             return self._res.error(exc)
-    #     return decorator
 
     def check_role(self, f, role):
         def decorator(user_id=None):
@@ -139,16 +97,78 @@ class AuthenticationHandler:
         return token_split[1]
 
 
-def start_keycloak() -> KeycloakOpenID:
-    """
-    Start Keycloak client according to credentials stored in env variables.
-    """
+def verify_token(token):
+    
+    token_header = jwt.get_unverified_header(token)
+    token_unverified = jwt.decode(token, verify=False)
+    
+    user_verified = verify_user(token_unverified['email'])
+    issuer_verified = verify_oidc_issuer(token_unverified['iss'] + "/.well-known/openid-configuration")
+    
+    token_verified = None
+    if user_verified and issuer_verified:
+        jwks = get_auth_jwks(token_unverified['iss'] + "/.well-known/openid-configuration")
+        
+        for key in jwks['keys']:
+            if key['kid'] == token_header['kid']:
+                public_key = rsa_pem_from_jwk(key)
+        
+        token_verified = jwt.decode(token,
+                             public_key,
+                             verify=True,
+                             algorithms=token_header['alg'],
+                             audience=token_unverified['azp'],
+                             issuer=token_unverified['iss'])
+        
+    return token_verified
+    
 
-    # Instantiate keycloak client
-    keycloak_openid = KeycloakOpenID(
-        server_url=environ.get('OIDC_URL'),
-        realm_name=environ.get('OIDC_REALM'),
-        client_id=environ.get('OIDC_CLIENT_ID'),
-        client_secret_key=environ.get('OIDC_CLIENT_SECRET'))
+def verify_user(user_email):
+    """
+    
+    """
+    
+    # NB this info should be stored in a database
+    user_verified = False
+    if user_email.split('@')[-1] == 'eodc.eu':
+        user_verified = True
+    else:
+        with open(os.path.join(os.environ.get('OIDC_DIR'), 'users_external.json'), 'r') as f:
+            users_list = json.load(f)
+            if user_email in users_list['users']:
+                user_verified = True
+    
+    return user_verified
+    
 
-    return keycloak_openid
+def verify_oidc_issuer(issuer):
+    """
+    
+    """
+    
+    # NB this info should be stored in a database
+    issuer_verified = False
+    if issuer in os.environ.get("OPENID_DISCOVERY"):
+        issuer_verified = True
+    else:
+        # NB this info should be stored in a database
+        with open(os.path.join(os.environ.get('OIDC_DIR'), 'issuers_external.json'), 'r') as f:
+            oidc_list = json.load(f)
+            if issuer in oidc_list['issuers']:
+                issuer_verified = True
+                
+    return issuer_verified
+    
+    
+    
+def get_auth_jwks(oidc_well_known_url):
+    
+    jwks = None    
+    response = requests.get(oidc_well_known_url)
+    if response.status_code == 200:
+        jwks_uri = response.json()['jwks_uri']
+        response = requests.get(jwks_uri)
+        if response.status_code == 200:
+            jwks = response.json()
+    
+    return jwks
