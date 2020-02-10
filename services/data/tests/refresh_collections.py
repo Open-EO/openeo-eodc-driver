dc94659eadbc57406838b47a840aaee8dcb00d8b
sherrmann <sophie.herrmann@eodc.eu>
2020-02-10 15:41:39 +0100

    Add simple collections test
    Signed-off-by: sherrmann <sophie.herrmann@eodc.eu>

---
+++ services/data/tests/refresh_collections.py
@@ -0,0 +1,25 @@
+import os
+
+import requests
+
+
+def test_collections():
+    backend_url = "http://127.0.0.1:3000"
+    basic_auth_url = backend_url + "/credentials/basic"
+    collections_url = backend_url + "/collections"
+
+    auth_response = requests.get(basic_auth_url, auth=(os.environ.get('USERNAME'), os.environ.get('PASSWORD')))
+    auth_header = {'Authorization': 'Bearer ' + auth_response.json()['token']}
+
+    response_get_all = requests.get(url=collections_url)
+    assert response_get_all.ok
+    response_post = requests.post(url=collections_url, headers=auth_header)
+    assert response_post.ok
+    response_get_all = requests.get(url=collections_url)
+    assert response_get_all.ok
+    response_get_single = requests.get(url=collections_url + "/s2a_prd_msil1c")
+    assert response_get_single.ok
+
+
+if __name__ == '__main__':
+    test_collections()
