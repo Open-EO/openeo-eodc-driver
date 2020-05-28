0b5df7e2092a9df5eca0595bc4c4e4d48b3a997f
sherrmann <sophie.herrmann@eodc.eu>
2020-05-28 14:01:08 +0200

    Add flake8 linter setup
    Signed-off-by: sherrmann <sophie.herrmann@eodc.eu>

--- services/capabilities/noxfile.py
+++ services/capabilities/noxfile.py
@@ -2,8 +2,8 @@ import nox
 from nox.sessions import Session
 # TODO think about dependency management
 
-locations = "capabilities", "tests", "noxfile.py"
-nox.options.sessions = "tests"
+locations = "capabilities", "tests", "noxfile.py"  # where to run flake8
+nox.options.sessions = "lint", "mypy", "tests"
 
 
 @nox.session(python=["3.6"])
@@ -17,3 +17,16 @@ def tests(session: Session) -> None:
         "nameko",
     )
     session.run("pytest", *args)
+
+
+@nox.session(python=["3.6"])
+def lint(session: Session) -> None:
+    args = session.posargs or locations
+    session.install(
+        "flake8",
+        "flake8-annotations",
+        "flake8-bugbear",
+        "flake8-bandit",
+        "flake8-import-order",  # think about import order style!
+    )
+    session.run("flake8", *args)
