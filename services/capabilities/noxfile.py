6c224b47217ce26ea79dd7dc494c38bf0f8db374
sophieherrmann <37064557+sophieherrmann@users.noreply.github.com>
2020-05-29 09:58:28 +0200

    Automate capabilities tests  (#100)
    * Initial setup with nox
    
    Signed-off-by: sherrmann <sophie.herrmann@eodc.eu>
    
    * Add description how to run tests with nox
    
    Signed-off-by: sherrmann <sophie.herrmann@eodc.eu>
    
    * Add flake8 linter setup
    
    Signed-off-by: sherrmann <sophie.herrmann@eodc.eu>
    
    * Fix flake8 in capabilities
    
    Signed-off-by: sherrmann <sophie.herrmann@eodc.eu>
    
    * Update run_test doc
    
    Signed-off-by: sherrmann <sophie.herrmann@eodc.eu>
    
    * Add static type checking with mypy
    
    Signed-off-by: sherrmann <sophie.herrmann@eodc.eu>
    
    * Fix type annotations in capabilities
    
    Signed-off-by: sherrmann <sophie.herrmann@eodc.eu>
    
    * Update run_tests.md
    
    Co-authored-by: Luca Foresta <luca.foresta@eodc.eu>

---
+++ services/capabilities/noxfile.py
@@ -0,0 +1,39 @@
+import nox
+from nox.sessions import Session
+# TODO think about dependency management
+
+locations = "capabilities", "tests", "noxfile.py"  # where to run flake8
+nox.options.sessions = "lint", "mypy", "tests"
+
+
+@nox.session(python=["3.6"])
+def tests(session: Session) -> None:
+    args = session.posargs or ["--cov"]
+    # no requirements needed for the package itself
+    session.install(
+        "pytest",
+        "pytest-cov",
+        "coverage[toml]",
+        "nameko",
+    )
+    session.run("pytest", *args)
+
+
+@nox.session(python=["3.6"])
+def lint(session: Session) -> None:
+    args = session.posargs or locations
+    session.install(
+        "flake8",
+        "flake8-annotations",
+        "flake8-bugbear",
+        "flake8-bandit",
+        "flake8-import-order",  # think about import order style!
+    )
+    session.run("flake8", *args)
+
+
+@nox.session(python=["3.6"])
+def mypy(session: Session) -> None:
+    args = session.posargs or locations
+    session.install("mypy")
+    session.run("mypy", *args)
--- services/capabilities/tests/conftest.py
+++ services/capabilities/tests/conftest.py
@@ -2,8 +2,8 @@
 # by pytest
 
 import sys
-from os.path import dirname
 from os.path import abspath
+from os.path import dirname
 
 root_dir = dirname(dirname(abspath(__file__)))
 sys.path.append(root_dir)
