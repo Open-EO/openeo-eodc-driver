e44bbd088eeb5388024b4a33e0cea09f0c623f56
Luca Foresta <luca.foresta@eodc.eu>
2020-06-04 16:52:49 +0200

    remove eodatareadrs_rpc
    - this micro-service existed only for the sync processing, now
    integrated in the jobs service

--- services/eodatareaders_rpc/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-until nc -z ${RABBIT_HOST} ${RABBIT_PORT}; do
-    echo "$(date) - waiting for rabbitmq..."
-    sleep 1
-done
-
-echo "$(date) - Started eodatareaders_rpc.service"
-nameko run --config config.yaml eodatareaders_rpc.service --backdoor 3000
