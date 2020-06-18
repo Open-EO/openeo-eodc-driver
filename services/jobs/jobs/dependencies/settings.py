f2bdcca0cc91780e3d016826f8d440e208af08f8
sherrmann <sophie.herrmann@eodc.eu>
2020-06-18 11:08:10 +0200

    Add required env vars for external packages in job service
    Signed-off-by: sherrmann <sophie.herrmann@eodc.eu>

--- services/jobs/jobs/dependencies/settings.py
+++ services/jobs/jobs/dependencies/settings.py
@@ -1,6 +1,8 @@
-from os import makedirs
+from os import makedirs, environ
 from os.path import isdir
+from urllib.error import URLError
 from urllib.parse import urlparse
+from urllib.request import Request, urlopen
 
 from dynaconf import Validator, settings
 
@@ -19,6 +21,17 @@ class SettingValidationUtils:
         result = urlparse(url)
         return all([result.scheme, result.netloc])
 
+    def check_url_is_reachable(self, url: str) -> bool:
+        try:
+            if url.lower().startswith('http'):
+                req = Request(url)
+                with urlopen(req) as resp:  # noqa
+                    return resp.status == 200
+            else:
+                return False
+        except URLError:
+            return False
+
 
 def initialise_settings() -> None:
     settings.configure(ENVVAR_PREFIX_FOR_DYNACONF="OEO")
@@ -30,5 +43,9 @@ def initialise_settings() -> None:
         Validator("AIRFLOW_DAGS", must_exist=True, condition=utils.check_create_folder),
         Validator("SYNC_DEL_DELAY", must_exist=True, is_type_of=int, condition=utils.check_positive_int),
         Validator("SYNC_RESULTS_FOLDER", must_exist=True, condition=utils.check_create_folder),
+        Validator("CSW_SERVER", must_exist=True, condition=utils.check_url_is_reachable)
     )
     settings.validators.validate()
+    # needed for eodc-openeo-bindings - should be removed once this is handled in a better way
+    environ["CSW_SERVER"] = settings.CSW_SERVER
+    environ["AIRFLOW_DAGS"] = settings.AIRFLOW_DAGS
