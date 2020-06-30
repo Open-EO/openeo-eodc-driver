1ef674822f3684a852207cc5082b67053e4ea7a8
sherrmann <sophie.herrmann@eodc.eu>
2020-06-30 16:19:48 +0200

    Separate job exceptions
    Signed-off-by: sherrmann <sophie.herrmann@eodc.eu>

--- services/jobs/jobs/exceptions.py
+++ services/jobs/jobs/exceptions.py
@@ -1,3 +1,59 @@
+import logging
+
+LOGGER = logging.getLogger('standardlog')
+
+
+class ServiceException(Exception):
+    """ServiceException raises if an exception occured while processing the
+    request. The ServiceException is mapping any exception to a serializable
+    format for the API gateway.
+    """
+
+    def __init__(self, code: int, user_id: str, msg: str, internal: bool = True, links: list = None) -> None:
+        if not links:
+            links = []
+
+        self._service = "jobs"
+        self._code = code
+        self._user_id = user_id
+        self._msg = msg
+        self._internal = internal
+        self._links = links
+        LOGGER.exception(msg, exc_info=True)
+
+    def to_dict(self) -> dict:
+        """Serializes the object to a dict.
+
+        Returns:
+            dict -- The serialized exception
+        """
+
+        return {
+            "status": "error",
+            "service": self._service,
+            "code": self._code,
+            "user_id": self._user_id,
+            "msg": self._msg,
+            "internal": self._internal,
+            "links": self._links
+        }
+
+
+class JobLocked(ServiceException):
+    """ JobLocked raised if job is queued / running when trying to modify it. """
+    def __init__(self, code: int, user_id: str, msg: str, internal: bool = False, links: list = None) -> None:
+        super(JobLocked, self).__init__(code, user_id, msg, internal, links)
+
+
+class JobNotFinished(ServiceException):
+    """ JobNotFinished raised if job is not finished but results are requested . """
+    def __init__(self, code: int, user_id: str, job_id: str, msg: str = None, internal: bool = False,
+                 links: list = None) -> None:
+        if not msg:
+            msg = f"Job {job_id} is not yet finished. Results cannot be accessed."
+        super().__init__(code, user_id, msg, internal, links)
+
+
 class BadRequest(Exception):
     """ BadRequest raises if the job was not found. """
     def __init__(self, msg: str = None) -> None:
--- services/jobs/jobs/service.py
+++ services/jobs/jobs/service.py
@@ -18,63 +18,13 @@ from nameko_sqlalchemy import DatabaseSession
 
 from .dependencies.airflow_conn import AirflowRestConnectionProvider
 from .dependencies.settings import initialise_settings
+from .exceptions import JobLocked, JobNotFinished, ServiceException
 from .models import Base, Job, JobStatus
 from .schema import JobCreateSchema, JobFullSchema, JobResultsBaseSchema, JobShortSchema
 
 service_name = "jobs"
 LOGGER = logging.getLogger('standardlog')
 initialise_settings()
-
-
-class ServiceException(Exception):
-    """ServiceException raises if an exception occured while processing the
-    request. The ServiceException is mapping any exception to a serializable
-    format for the API gateway.
-    """
-
-    def __init__(self, code: int, user_id: str, msg: str, internal: bool = True, links: list = None) -> None:
-        if not links:
-            links = []
-
-        self._service = service_name
-        self._code = code
-        self._user_id = user_id
-        self._msg = msg
-        self._internal = internal
-        self._links = links
-        LOGGER.exception(msg, exc_info=True)
-
-    def to_dict(self) -> dict:
-        """Serializes the object to a dict.
-
-        Returns:
-            dict -- The serialized exception
-        """
-
-        return {
-            "status": "error",
-            "service": self._service,
-            "code": self._code,
-            "user_id": self._user_id,
-            "msg": self._msg,
-            "internal": self._internal,
-            "links": self._links
-        }
-
-
-class JobLocked(ServiceException):
-    """ JobLocked raised if job is queued / running when trying to modify it. """
-    def __init__(self, code: int, user_id: str, msg: str, internal: bool = True, links: list = None) -> None:
-        super(JobLocked, self).__init__(code, user_id, msg, internal, links)
-
-
-class JobNotFinished(ServiceException):
-    """ JobNotFinished raised if job is not finished but results are requested . """
-    def __init__(self, code: int, user_id: str, job_id: str, msg: str = None, internal: bool = True,
-                 links: list = None) -> None:
-        if not msg:
-            msg = f"Job {job_id} is not yet finished. Results cannot be accessed."
-        super().__init__(code, user_id, msg, internal, links)
 
 
 class JobService:
