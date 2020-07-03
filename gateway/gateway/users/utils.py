59e2e504f5cdd1dbf49de91270fcfe3839b7d176
sherrmann <sophie.herrmann@eodc.eu>
2020-07-03 10:10:22 +0200

    Change user obj to support not set storage
    Signed-off-by: sherrmann <sophie.herrmann@eodc.eu>

--- gateway/gateway/users/utils.py
+++ gateway/gateway/users/utils.py
@@ -4,7 +4,7 @@ from .models import Profiles, Users
 
 
 def db_to_dict_user(db_user: Users, db_profile: Profiles) -> Dict[str, Any]:
-    return {
+    user = {
         "id": db_user.id,
         "role": db_user.role,
         "auth_type": str(db_user.auth_type),
@@ -18,3 +18,9 @@ def db_to_dict_user(db_user: Users, db_profile: Profiles) -> Dict[str, Any]:
         "created_at": db_user.created_at,
         "updated_at": db_user.updated_at,
     }
+    if db_user.storage:
+        user["storage"] = {
+            "free": db_user.storage.free,
+            "quota": db_user.storage.quota,
+        }
+    return user
