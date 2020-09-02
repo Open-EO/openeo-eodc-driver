9ae47197078b25407f5104b896025189d09890e8
Luca Foresta <luca.foresta@eodc.eu>
2020-09-02 09:00:32 +0200

    remove logic to parallelize DAGs
    - this is done via a specific task created by the eodc-bindings

--- airflow/plugins/eodatareaders_operators.py
+++ airflow/plugins/eodatareaders_operators.py
@@ -3,12 +3,9 @@ import glob
 import logging
 
 from airflow.models import BaseOperator
-from airflow.operators.python_operator import PythonOperator
 from airflow.plugins_manager import AirflowPlugin
 from airflow.utils.decorators import apply_defaults
-from airflow.sensors.external_task_sensor import ExternalTaskSensor
 from eodatareaders.eo_data_reader import EODataProcessor
-from eodc_openeo_bindings.job_writer.dag_writer import AirflowDagWriter
 
 log = logging.getLogger(__name__)
 
@@ -23,34 +20,6 @@ class eoDataReadersOp(BaseOperator):
 
     def execute(self, context):
         _ = EODataProcessor(filepaths=self.input_filepaths, user_params=self.input_params, dc_filepaths=self.input_dc_filepaths)
-
-
-# def parallelise_dag(job_id, user_name, process_graph_json, job_data, process_def):
-#     """
-# 
-#     """
-# 
-#     writer = AirflowDagWriter()
-#     domain = writer.get_domain(job_id=job_id,
-#                                user_name=user_name,
-#                                proces_graph_json=process_graph_json,
-#                                job_data=job_data,
-#                                process_dfs=process_defs)
-#     writer.rewrite_and_move_job(domain)
-# 
-# 
-# class ParalleliseDagOp(PythonOperator):
-#     @apply_defaults
-#     def __init__(self, job_id, user_name, process_graph_json, job_data, process_def, *args, **kwargs):
-#         super().__init__(python_callable=parallelise_dag,
-#                          op_kwargs={
-#                             'job_id': job_id,
-#                             'user_name': user_name,
-#                             'process_graph_json': process_graph_json,
-#                             'job_data': job_data,
-#                             'process_def': process_def
-#                             }
-#                         )
 
 
 class eoDataReadersPlugin(AirflowPlugin):
