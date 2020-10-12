ad36f6335df19f0d45c0ed3aa390012871f573ae
sherrmann <sophie.herrmann@eodc.eu>
2020-10-12 09:25:34 +0200

    Copy script to absolute path
    Signed-off-by: sherrmann <sophie.herrmann@eodc.eu>

--- setup_scripts/add_init_users.sh
+++ setup_scripts/add_init_users.sh
@@ -1,4 +1,4 @@
-docker cp setup_scripts/add_init_users.py oeo-users-v1.0:/usr/src
+docker cp setup_scripts/add_init_users.py oeo-users-v1.0:/usr/src/add_init_users.py
 docker exec -it oeo-users-v1.0 bash
 python /usr/src/add_init_users.py
 rm /usr/src/add_init_users.py
