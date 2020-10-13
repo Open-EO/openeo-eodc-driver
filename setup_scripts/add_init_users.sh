681abe9f181ceee5a758c52a5dfa29b9bfe3554f
sherrmann <sophie.herrmann@eodc.eu>
2020-10-13 14:23:19 +0200

    Combine docker commands
    Signed-off-by: sherrmann <sophie.herrmann@eodc.eu>

--- setup_scripts/add_init_users.sh
+++ setup_scripts/add_init_users.sh
@@ -3,8 +3,4 @@
 docker cp setup_scripts/sample-auth.sh oeo-users-v1.0:/usr/src/app/sample-auth.sh
 docker cp setup_scripts/add_init_users.py oeo-users-v1.0:/usr/src/app/add_init_users.py
 
-docker exec oeo-users-v1.0 source /usr/src/app/sample-auth.sh
-docker exec oeo-users-v1.0 python /usr/src/app/add_init_users.py
-
-docker exec oeo-users-v1.0 rm /usr/src/app/add_init_users.py
-docker exec oeo-users-v1.0 rm /usr/src/app/sample-auth.sh
+docker exec bash oeo-users-v1.0 -c "source /usr/src/app/sample-auth.sh ; python /usr/src/app/add_init_users.py ; rm /usr/src/app/add_init_users.py ; rm /usr/src/app/sample-auth.sh"
