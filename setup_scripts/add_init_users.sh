16a5d8ce9d026bd3caf29d48d98bfbf46d2e21a1
sherrmann <sophie.herrmann@eodc.eu>
2020-10-13 12:57:41 +0200

    Add shebang and remove -it
    Signed-off-by: sherrmann <sophie.herrmann@eodc.eu>

--- setup_scripts/add_init_users.sh
+++ setup_scripts/add_init_users.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 docker cp setup_scripts/add_init_users.py oeo-users-v1.0:/usr/src/app/add_init_users.py
-docker exec -it oeo-users-v1.0 python /usr/src/app/add_init_users.py
-docker exec -it oeo-users-v1.0 rm /usr/src/app/add_init_users.py
+docker exec oeo-users-v1.0 python /usr/src/app/add_init_users.py
+docker exec oeo-users-v1.0 rm /usr/src/app/add_init_users.py
