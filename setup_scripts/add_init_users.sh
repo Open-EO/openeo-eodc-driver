a062acc13f8ecc5526906c3c891ab7323cb59f5c
sherrmann <sophie.herrmann@eodc.eu>
2020-10-13 14:06:14 +0200

    Add sample-auth in users container
    Signed-off-by: sherrmann <sophie.herrmann@eodc.eu>

--- setup_scripts/add_init_users.sh
+++ setup_scripts/add_init_users.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+docker cp setup_scripts/sample-auth.sh oeo-users-v1.0:/usr/src/app/sample-auth.sh
 docker cp setup_scripts/add_init_users.py oeo-users-v1.0:/usr/src/app/add_init_users.py
+
+docker exec oeo-users-v1.0 source /usr/src/app/sample-auth.sh
 docker exec oeo-users-v1.0 python /usr/src/app/add_init_users.py
+
 docker exec oeo-users-v1.0 rm /usr/src/app/add_init_users.py
+docker exec oeo-users-v1.0 rm /usr/src/app/sample-auth.sh
