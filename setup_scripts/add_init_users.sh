739b31356a77afb8cbfd85bc7a4426a2ba3574f1
sherrmann <sophie.herrmann@eodc.eu>
2020-10-13 14:43:17 +0200

    Remove source cmd
    Signed-off-by: sherrmann <sophie.herrmann@eodc.eu>

--- setup_scripts/add_init_users.sh
+++ setup_scripts/add_init_users.sh
@@ -3,4 +3,5 @@
 docker cp setup_scripts/sample-auth.sh oeo-users-v1.0:/usr/src/app/sample-auth.sh
 docker cp setup_scripts/add_init_users.py oeo-users-v1.0:/usr/src/app/add_init_users.py
 
-docker exec bash oeo-users-v1.0 -c "source /usr/src/app/sample-auth.sh ; python /usr/src/app/add_init_users.py ; rm /usr/src/app/add_init_users.py ; rm /usr/src/app/sample-auth.sh"
+docker exec oeo-users-v1.0 . /usr/src/app/sample-auth.sh
+docker exec oeo-users-v1.0 python /usr/src/app/add_init_users.py
